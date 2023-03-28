eb7b0b791ef17a639719d787cab7b82b8fa20e00
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-29 00:07:25 +0530

    String formatting
    sting formatting

--- JAVA_Introduction_HackerRank/StringsOne.java
+++ JAVA_Introduction_HackerRank/StringsOne.java
@@ -52,7 +52,17 @@ public class StringsOne {
         in.close();
         System.out.println(S.substring(start,end));
     }
+    public void stringFormatting(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+             System.out.printf("%-15s%03d%n",s1,x);
+        }
+        System.out.println("================================");
+    }
 }
-
-
+ 
 
